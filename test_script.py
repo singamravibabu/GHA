655cffb139bde55db11732681d76ffe2160cb597
Singam Ravi Babu <115613718+singamravibabu@users.noreply.github.com>
2025-01-27 20:24:02 +0530

    Create test_script.py

---
+++ test_script.py
@@ -0,0 +1,8 @@
+# test_script.py
+
+def test_example():
+  assert 1 + 1 == 2, "Math is broken!"
+
+def test_message():
+  message = "Hello, GitHub Actions!"
+  assert message == "Hello, GitHub Actions!", "Message does not match"
